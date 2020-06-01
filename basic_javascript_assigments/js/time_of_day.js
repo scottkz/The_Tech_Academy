a7fe182cb07e09425b887f77794b24f1399891a6
Scott Katzelnick <scott@katzelnick.com>
2020-06-01 19:59:21 -0400

    One Page Website Project

--- basic_javascript_assigments/js/time_of_day.js
+++ basic_javascript_assigments/js/time_of_day.js
@@ -1,12 +1,26 @@
-function TimeOfDay() {
-    var time = new Date().getHours();
-    var reply;
-    if (time < 12 == time > 0) {
-        reply = "It's morning time!"
-    } else if (time > 12 == time < 18) {
-        reply = "It's afternoon time!";
-    } else {
-        reply = "It's evening time!"
-    }
-    document.getElementById("time").innerHTML = reply;
+function displayTime() {
+  let militaryTime = new Date().getHours();
+  let time = (new Date().getHours() + 24) % 12;
+  let minutes = new Date().getMinutes();
+  let reply;
+
+  if (militaryTime < 12 == militaryTime >= 0) {
+    reply = ' in the morning!';
+  } else if (militaryTime >= 12 == militaryTime < 16) {
+    reply = ' in the afternoon !';
+  } else if (militaryTime >= 16 == militaryTime < 20) {
+    reply = ' in the evening!';
+  } else {
+    reply = ' at night!';
+  }
+
+  if (militaryTime < 12) {
+    var timeOfDay = 'am';
+  } else {
+    var timeOfDay = 'pm';
+  }
+
+  document.getElementById(
+    'time'
+  ).innerHTML = `It's ${time}:${minutes} ${timeOfDay}${reply}!`;
 }
