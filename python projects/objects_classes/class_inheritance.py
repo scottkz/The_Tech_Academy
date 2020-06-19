6fabfa49d6a933d115fe4f45fc2403d4a9b0efc7
Scott Katzelnick <scott@katzelnick.com>
2020-06-19 19:04:14 -0400

    New classes and objects projects

---
+++ python projects/objects_classes/class_inheritance.py
@@ -0,0 +1,15 @@
+
+class Animal:
+    genus = ''
+    species = ''
+    age = 0
+
+
+class Canine(Animal):
+    breed = ''
+    color = ''
+
+
+class Feline(Animal):
+    mix = ''
+    temperament = ''
