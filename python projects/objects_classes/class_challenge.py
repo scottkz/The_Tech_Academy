96431d78b8796a44fe3f68a60a5296ab3dc53366
Scott Katzelnick <scott@katzelnick.com>
2020-06-20 03:36:29 -0400

    Added polymorphism assignment
    Signed-off-by: Scott Katzelnick <scott@katzelnick.com>

--- python projects/objects_classes/class_challenge.py
+++ python projects/objects_classes/class_challenge.py
@@ -1,31 +1,58 @@
-# Parent class
-class Student:
-    def __init__(self, name, grade, gpa, school, district):
-        self.name = "Alex Smith"
-        self.grade = 10
-        self.gpa = 3.55
-        self.school = "Palomena High School"
-        self.district = "Orange County School District"
+import datetime
 
-    def get_info(self):
-        print(
-            "\n{} is a student at {} in the {}. {} is in the {}th grade with a GPA of {}".format(
-                self.name,
-                self.school,
-                self.district,
-                self.name,
-                self.grade,
-                self.gpa,
-            )
+
+# Parent Class
+class Person:
+    name = ""
+    school = "Palomena High School"
+    district = "Orange County School District"
+    grade = 0
+    gpa = 0
+    employee_id = 0
+    hire_date = 0
+    pay_rate = 0
+
+    def get_student_info(self):
+        msg = "\n{} is a student at {} in the {}. {} is in the {}th grade with a GPA of {}.".format(
+            self.name,
+            self.school,
+            self.district,
+            self.name,
+            self.grade,
+            self.gpa,
         )
+        return msg
+
+    def get_employee_info(self):
+        msg = "\n{} is an employee at {} in the {}. {} was hired on {} and currently makes {}/hour.".format(
+            self.name,
+            self.school,
+            self.district,
+            self.name,
+            self.hire_date,
+            self.pay_rate,
+        )
+        return msg
+
+
+# Child class
+class Student(Person):
+    name = "Alex Smith"
+    grade = 10
+    gpa = 3.55
+
+
+# Child Class
+class Employee(Person):
+    name = "Edgar Warez"
+    employee_id = ""
+    hire_date = datetime.date(2015, 8, 11)
+    pay_rate = 19
 
 
 if __name__ == "__main__":
-    student = Student(
-        name=Student,
-        grade=Student,
-        gpa=Student,
-        school=Student,
-        district=Student,
-    )
-    student.get_info()
+    stud = Student()
+    print(stud.get_student_info())
+
+    emp = Employee()
+    print(emp.get_employee_info())
---
+++ python projects/objects_classes/polymorphism_assignment.py
@@ -0,0 +1,111 @@
+# The Tech Academy - Software Developer BootCamp
+#
+# Python Version: 3.8.3
+#
+# Author: Scott Katzelnick
+#
+# Purpose:
+#
+# POLYMORPHISM ASSIGNMENT
+# Create two classes that inherit from another class.
+#
+# 1. Each child should have at least two of their own attributes.
+#
+# 2. The parent class should have at least one method (function).
+#
+# 3. Both child classes should utilize polymorphism on the parent class method.
+#
+# 4. Upload code to GitHub for review
+
+
+# Parent Class - Mammals
+class Mammals:
+    def __init__(self, species, vertebrate, invertebrate, movement, sex):
+        self.sname = species
+        self.vtype = vertebrate
+        self.invtype = invertebrate
+        self.move = movement
+        self.sextype = sex
+
+    # Parent class method
+    def get_info(self):
+        print(
+            "\nSpecies: {}\nVertebrate: {}\nInvertebrate: {}\nMovement Type: {}\nSex: {}".format(
+                self.sname, self.vtype, self.invtype, self.move, self.sextype
+            )
+        )
+
+
+# First child class of Mammals - Humans
+class Humans(Mammals):
+    def __init__(
+            self, species, vertebrate, invertebrate, movement, sex, name, age
+    ):
+        super().__init__(species, vertebrate, invertebrate, movement, sex)
+        self.fname = name
+        self.humanage = age
+
+    # Overrides parent method get_info() with polymorphism
+    def get_info(self):
+        print(
+            "\nName: {}\nAge: {}\nSpecies: {}\nVertebrate: {}\nInvertebrate: {}\nMovement Type: {}\nSex: {}".format(
+                self.fname,
+                self.humanage,
+                self.sname,
+                self.vtype,
+                self.invtype,
+                self.move,
+                self.sextype,
+            )
+        )
+
+
+# Second child class of Mammals - Bear
+class Bears(Mammals):
+    def __init__(
+            self, species, vertebrate, invertebrate, movement, sex, color, habitat
+    ):
+        super().__init__(species, vertebrate, invertebrate, movement, sex)
+        self.bcolor = color
+        self.bhabitat = habitat
+
+    # Overrides parent method get_info() with polymorphism
+    def get_info(self):
+        print(
+            "\nSpecies: {}\nVertebrate: {}\nInvertebrate: {}\nMovement Type: {}\nSex: {}\nColor: {}\nHabitat: {}".format(
+                self.sname,
+                self.vtype,
+                self.invtype,
+                self.move,
+                self.sextype,
+                self.bcolor,
+                self.bhabitat,
+            )
+        )
+
+
+if __name__ == "__main__":
+    mam = Mammals(
+        "Balaenoptera musculus/Blue Whale",
+        True,
+        False,
+        "Pectoral Flippers",
+        "Male",
+    )
+    mam.get_info()
+
+    hum = Humans(
+        "Homo sapiens/Human", True, False, "Bipedal", "Female", "Nicole", 24
+    )
+    hum.get_info()
+
+    bear = Bears(
+        "Ursus arctos/Brown Bear",
+        True,
+        False,
+        "Quadrupedal",
+        "Male",
+        "Dark Brown",
+        "Mountain Woodlands",
+    )
+    bear.get_info()
